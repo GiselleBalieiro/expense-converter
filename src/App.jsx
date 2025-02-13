7b8b8d61959fb6162f5ef16030629360c82dc3be
GiselleBalieiro <gisellefbalieiro@outlook.com>
2025-02-12 21:10:42 -0300

    chore: salvar despesas no localStorage ao atualizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ExpenseForm from './components/ExpenseForm/ExpenseForm'
 import ExpenseList from './components/ExpenseList/ExpenseList'
 import Button from './components/Button/Button';
@@ -12,6 +12,18 @@ function App() {
   const [ exchangeRates, setExchangeRates ] = useState ({});
   const [ selectedCurrency, setSelectedCurrency ] = useState ('')
   const [ showForm, setShowForm ] = useState (false);
+
+  useEffect(() => {
+    const savedExpenses = localStorage.getItem("expenses");
+    if (savedExpenses) {
+      setExpenses(JSON.parse(savedExpenses));
+    }
+  },[]);
+
+  useEffect(() => {
+    if(expenses.length > 0)
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
  
   return (
     <>
